ce6cd8bd216d50a56f3324445f83c4a739b0bf38
seanmccrdy <sean.mccurdy@utoront.ca>
2017-01-03 21:43:47 -0500

    remove duplicates, changed output report, created new indexing, created a precision output

--- model_training.py
+++ model_training.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import sys
+import string as stringr
 import pandas as pd
 import numpy as np
 import time as t
@@ -30,14 +31,19 @@ print()
 print("libraries and functions loaded...")
 
 data2 = pd.read_csv("train_data/merge_arysm_sig_summary.csv",
-                    encoding = "latin1",
-                    index_col = 1).iloc[:,1:]
+                    encoding = "latin1")
+data2.geo_id = [s.translate(str.maketrans('','',stringr.punctuation+' ')).lower() for s in data2.geo_id]
+data2.index = data2.filenames.values +"-"+data2.geo_id.values
+data2 = data2.iloc[:,2:]
+data2 = data2.groupby(data2.index).first()
 
 print("imported train data...")
 
-data = pd.read_csv( "train_data/merge_arysm_sig_summary_supplement.csv",
-                    encoding = "latin1",
-                    index_col = 1).iloc[:,1:]
+data = pd.read_csv( "train_data/merge_arysm_sig_summary_supplement3_withCCL.csv",
+                    encoding = "latin1")
+data.geo_id = [s.translate(str.maketrans('','',stringr.punctuation+' ')).lower() for s in data.geo_id]
+data.index = data.filenames.values +"-"+data.geo_id.values
+data = data.iloc[:,2:]
 
 print("imported train data supplment...")
 
@@ -50,13 +56,15 @@ merged_data.dropna( axis = 1,
                     inplace = True)
 
 meta = pd.read_csv("train_data/meta_master.csv")
-meta.index = meta.geo_id
+meta.geo_id = [s.translate(str.maketrans('','',stringr.punctuation+' ')).lower() for s in meta.geo_id]
+meta.index = meta.filenames.values +"-"+meta.geo_id.values
+
 print("imported meta data...")
 
 y = meta.loc[merged_data.index.values].path_simple #keeps same order as transcriptome data
 x = merged_data
 x.index = merged_data.index.values
-low_freq_label_filter = y.isin(y.value_counts()[y.value_counts()>40].index.values)
+low_freq_label_filter = y.isin(y.value_counts()[y.value_counts()>=35].index.values)
 y = y[low_freq_label_filter]
 x = x[low_freq_label_filter]
 x = x.as_matrix() #input x must be a numpy array for the classifier
@@ -133,6 +141,9 @@ valid_precision = precision_score(y_true = y_valid.argmax(axis=1),
                                   y_pred = (model.predict(x_valid)).argmax(axis=1),
                                   average = "weighted")
 
+
+
+
 #feature/output labels
 if not os.path.exists("inputs"):
     os.makedirs("inputs")
@@ -160,7 +171,8 @@ print("Algorithm: ","V"+ver_num)
 print("Date: ",str(t.strftime("%Y-%m-%d")))
 print()
 print("Function: Outputs the probability of various diseases given gene expression data")
-print("Sample Type:  Human Patient Samples")
+print("Cross-Platform Normalization Method: arysm")
+print("Sample Type:  Human Patient RNA Samples")
 print("Conditions Evaluated: ",y_valid.shape[1])
 print()
 print("-----------Statistics------------")
@@ -179,7 +191,7 @@ print("Train Precision:        {0:.3f}".format(train_precision))
 print("Test Precision:         {0:.3f}".format(test_precision))
 print("Validation Precision:   {0:.3f}".format(valid_precision))
 print()
-print("Notable Sample Sources: GTEx, TCGA, ADNI, ArrayExpress, Gene Expression Omnibus (GEO)")
+print("Notable Sample Sources: GTEx, TCGA, ADNI, CCLE, CGP, CLCGP, Genentech, ArrayExpress, Gene Expression Omnibus (GEO)")
 sys.stdout.close()
 sys.stdout = temp
 
@@ -187,3 +199,13 @@ print("saving model architecture and weights...")
 model.save("model/"+model_filename+".h5")
 print("model"+model_filename+".h5 saved...")
 print("program complete...")
+
+#individual model
+DataFrame([precision_score(y_true = y_train.argmax(axis=1),
+                y_pred = (model.predict(x_train)).argmax(axis=1),average= None),
+precision_score(y_true = y_test.argmax(axis=1),
+                y_pred = (model.predict(x_test)).argmax(axis=1),average= None),
+precision_score(y_true = y_valid.argmax(axis=1),
+                y_pred = (model.predict(x_valid)).argmax(axis=1),average= None)],
+                index=["train","test","validation"],
+                columns=Series(y_factorized[1])).T.to_csv("output/model_precision_results.csv")
